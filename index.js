fe064abff726a695744e4a761ee1625b01a4a6e6
Kasanstore7 <114809701+Kasanstore7@users.noreply.github.com>
2023-02-03 09:16:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('MultiDevice\nOscar', {
+CFonts.say('MultiDevice\nSaxia', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
